37424bdd50eb2ef10a4fee58aeeb8a36af931ed4
Sidharth Arya <sidharth.arya@solulever.com>
2026-03-17 23:12:51 +0530

    perf: using orjson by default

--- python/dapil/responses.py
+++ python/dapil/responses.py
@@ -39,21 +39,35 @@ from datetime import datetime, date
 from uuid import UUID
 from enum import Enum
 
+try:
+    import orjson
+except ImportError:
+    orjson = None
+
+def orjson_default(obj: Any) -> Any:
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    if isinstance(obj, UUID):
+        return str(obj)
+    if isinstance(obj, Enum):
+        return obj.value
+    if hasattr(obj, "dict") and callable(obj.dict):
+        return obj.dict()
+    if hasattr(obj, "model_dump") and callable(obj.model_dump):
+        return obj.model_dump()
+    raise TypeError(f"Object of type {obj.__class__.__name__} is not JSON serializable")
+
 class DapilJSONEncoder(json.JSONEncoder):
     def default(self, obj: Any) -> Any:
-        if isinstance(obj, (datetime, date)):
-            return obj.isoformat()
-        if isinstance(obj, UUID):
-            return str(obj)
-        if isinstance(obj, Enum):
-            return obj.value
-        if hasattr(obj, "dict") and callable(obj.dict):
-            return obj.dict()
-        if hasattr(obj, "model_dump") and callable(obj.model_dump):
-            return obj.model_dump()
-        return super().default(obj)
+        try:
+            return orjson_default(obj)
+        except TypeError:
+            return super().default(obj)
 
-def json_dumps(obj: Any, **kwargs: Any) -> str:
+def json_dumps(obj: Any, **kwargs: Any) -> Union[str, bytes]:
+    if orjson:
+        # orjson.dumps returns bytes
+        return orjson.dumps(obj, default=orjson_default)
     return json.dumps(obj, cls=DapilJSONEncoder, **kwargs)
 
 class JSONResponse(Response):
